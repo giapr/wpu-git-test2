3a89629fb95c5d882db5c0f5a0778f0e6c55b6f9
giapr <anggiaprillianti04@gmail.com>
2021-08-10 19:45:41 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem corporis vel recusandae accusamus officia veniam quisquam. Iure rem sed, non perspiciatis dolores mollitia quas. Perferendis aut expedita iure velit vel?
+</body>
+
+</html>
